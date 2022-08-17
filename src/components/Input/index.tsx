ba54194549ef79c54d4410d573c44d607e6d8441
Brunno Hofmann <brunnohofmann@hotmail.com>
2022-08-17 15:30:32 -0300

    Adds Input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {TextInput, TextInputProps} from 'react-native';
+
+const InputText = (props: TextInputProps) => {
+  return <TextInput {...props} />;
+};
+
+export default InputText;
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components/native';
+
+export const FormField = styled.View`
+  margin: 16px 0;
+`;
