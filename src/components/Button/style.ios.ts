fda9cf71e05c55fcf4c5e510ace58a734b327b03
Brunno Hofmann <brunnohofmann@hotmail.com>
2022-08-17 15:30:09 -0300

    Adds Button component

---
+++ src/components/Button/style.ios.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components/native';
+
+export const ButtonContainer = styled.Pressable`
+  background-color: ${({theme}) =>
+    theme.components.button.primary.backgroundColor};
+  border-radius: 8px;
+  padding: 16px;
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
+  justify-content: center;
+`;
+
+export const ActivityIndicatorContainer = styled.View`
+  padding-right: 8px;
+`;
+
+export const ButtonText = styled.Text`
+  color: ${({theme}) => theme.components.button.primary.textColor};
+  font-size: 16px;
+  text-align: center;
+`;
