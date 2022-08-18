d5acddacb9fef89eb8d33df39ce4e5cfce0f9296
Brunno Hofmann <brunnohofmann@hotmail.com>
2022-08-18 10:39:13 -0300

    Shwon Header on Home Screen

--- src/screens/Home/Fakitters/index.tsx
+++ src/screens/Home/Fakitters/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import {Text} from 'react-native';
+import Header from '../../../components/Header';
 
 const FakittersScreen = () => {
-  return <Text>Fakitters</Text>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 
 export default FakittersScreen;
