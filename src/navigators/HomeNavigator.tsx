1fb259c8da83c6e15af3f72daecdc497fb15759f
Brunno Hofmann <brunnohofmann@hotmail.com>
2022-08-18 09:42:47 -0300

    Stylizes bottom tab bar

--- src/navigators/HomeNavigator.tsx
+++ src/navigators/HomeNavigator.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import FakittersScreen from '../screens/Home/Fakitters';
+import HomeIcon from '../assets/img/home-icon.svg';
+import {ThemeContext} from 'styled-components/native';
 
 type HomeNavigatorScreenType = {
   FakittersScreen: undefined;
@@ -9,9 +11,20 @@ type HomeNavigatorScreenType = {
 const {Navigator, Screen} = createBottomTabNavigator<HomeNavigatorScreenType>();
 
 const HomeNavigator = () => {
+  const theme = React.useContext(ThemeContext);
+
   return (
-    <Navigator>
-      <Screen name="FakittersScreen" component={FakittersScreen} />
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {position: 'absolute', backgroundColor: theme.primary},
+      }}>
+      <Screen
+        options={{tabBarIcon: () => <HomeIcon fill="#ffffff" />}}
+        name="FakittersScreen"
+        component={FakittersScreen}
+      />
     </Navigator>
   );
 };
